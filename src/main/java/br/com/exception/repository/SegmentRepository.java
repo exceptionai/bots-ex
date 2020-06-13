d4f3876628d3449322692697d7ab2053b8d9d002
Vanessa Queiroz <vanessacaqz@gmail.com>
2020-06-13 18:39:42 -0300

    rebase and import models

--- src/main/java/br/com/exception/repository/SegmentRepository.java
+++ src/main/java/br/com/exception/repository/SegmentRepository.java
@@ -2,4 +2,6 @@ package br.com.exception.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import br.com.exception.model.SegmentModel;
+
 public interface SegmentRepository extends JpaRepository<SegmentModel, Long> {}
