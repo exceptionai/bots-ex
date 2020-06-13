2e35ee1c7fba8918f64484f70720bde8a860eaf2
ZangiefWins <fakesupremo2015@hotmail.com>
2020-06-13 18:18:09 -0300

    BotController

---
+++ src/main/java/br/com/exception/controller/BotController.java
@@ -0,0 +1,92 @@
+package br.com.exception.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.com.exception.model.BotModel;
+
+@Controller
+@RequestMapping("/bot")
+public class BotController {
+	
+	private static final String BOT_FOLDER = "bot/";
+	
+	//@Autowired
+	//public BotRepository repository;
+	
+	@GetMapping("/form")
+	public String open(@RequestParam String page, 
+					   @RequestParam(required = false) Long id,
+					   @ModelAttribute("botModel") BotModel botModel, 
+					   Model model) {
+		
+		if("bot-editar".equals(page)) {
+			//model.addAttribute("botModel", repository.findById(id).get());
+		}
+		
+		return BOT_FOLDER + page;
+	}
+	
+	@GetMapping()
+	public String findAll(Model model) {
+
+		//model.addAttribute("bots", repository.findAll());
+		return BOT_FOLDER +  "bots";
+	}
+
+	@GetMapping("/{id}")
+	public String findById(@PathVariable("id") long id, Model model) {
+		
+		//model.addAttribute("bot", repository.findById(id).get());
+		return BOT_FOLDER +  "bot-detalhe";
+	}
+	
+	@PostMapping()
+	public String save(@Valid BotModel botModel, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+		
+		if(bindingResult.hasErrors()) {
+			return BOT_FOLDER + "bot-novo";
+		}
+		
+		//repository.save(botModel);
+		redirectAttributes.addFlashAttribute("messages", "Bot cadastrado com sucesso!");
+		
+		return "redirect:/bot";
+	}
+	
+	@PutMapping("/{id}")
+	public String update(@PathVariable("id") long id, @Valid BotModel botModel, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+		
+		if(bindingResult.hasErrors()) {
+			return BOT_FOLDER + "bot-editar";
+		}
+		
+		botModel.setIdBot(id);
+		//repository.save(botModel);
+		redirectAttributes.addFlashAttribute("messages", "Bot alterado com sucesso!");
+		
+		return "redirect:/bot";
+	}
+	
+	@DeleteMapping("/{id}")
+	public String deleteById(@PathVariable("id") long id, RedirectAttributes redirectAttributes) {
+		
+		//repository.deleteById(id);
+		redirectAttributes.addFlashAttribute("messages", "Bot excluído com sucesso!");
+
+		return "redirect:/bot";
+	}
+
+}
