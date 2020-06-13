2eb72ffc701e957cfab3022c52dd2cd9f128c610
ZangiefWins <fakesupremo2015@hotmail.com>
2020-06-13 18:26:52 -0300

    SegmentController

--- src/main/java/br/com/exception/controller/BotController.java
+++ src/main/java/br/com/exception/controller/BotController.java
@@ -2,6 +2,7 @@ package br.com.exception.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
