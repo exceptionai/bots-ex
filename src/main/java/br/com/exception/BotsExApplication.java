8bdaf48d10c0559878fca58b12f5e50bc324251c
ZangiefWins <fakesupremo2015@hotmail.com>
2020-06-13 13:15:31 -0300

    Initial commit

---
+++ src/main/java/br/com/exception/BotsExApplication.java
@@ -0,0 +1,13 @@
+package br.com.exception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotsExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotsExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/exception/BotsExApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotsExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
